dfddf0361f0ebfacea4ea943c5d31e4816a2e102
YNoName1236 <2324494322@qq.com>
2021-08-17 15:13:42 +0800

    confirm commit in MyService.java

--- app/src/main/java/com/hui/realservicetest/MainActivity.java
+++ app/src/main/java/com/hui/realservicetest/MainActivity.java
@@ -2,13 +2,18 @@ package com.hui.realservicetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private Button start_service_btn,stop_service_btn;
+    private Button start_service_btn,stop_service_btn,bind_service_btn,unbind_service_btn;
+    private MyService.DownloadBinder downloadBinder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +22,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         stop_service_btn=findViewById(R.id.stop_service);
         start_service_btn.setOnClickListener(this);
         stop_service_btn.setOnClickListener(this);
+        bind_service_btn=findViewById(R.id.main_btn_bind);
+        unbind_service_btn=findViewById(R.id.main_btn_unbind);
+        bind_service_btn.setOnClickListener(this);
+        unbind_service_btn.setOnClickListener(this);
     }
 
+    private ServiceConnection connection=new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder= (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -29,6 +52,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.stop_service:
                 Intent stop_service_intent=new Intent(this,MyService.class);
                 stopService(stop_service_intent);//停止服务
+                break;
+            case R.id.main_btn_bind:
+                Intent bind_service_intent=new Intent(this,MyService.class);
+                bindService(bind_service_intent,connection,BIND_AUTO_CREATE);//绑定服务
+                break;
+            case R.id.main_btn_unbind:
+                unbindService(connection);//解绑服务
                 break;
             default:
                 break;
--- app/src/main/java/com/hui/realservicetest/MyService.java
+++ app/src/main/java/com/hui/realservicetest/MyService.java
@@ -2,27 +2,29 @@ package com.hui.realservicetest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
+
 public class MyService extends Service {
     private static final String TAG = "MyService";
+    private DownloadBinder mBinder = new DownloadBinder();
+
     public MyService() {
     }
+
     @Override
     public void onCreate() {
         super.onCreate();
         Log.e(TAG, "onCreate: MyService");
     }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.e(TAG, "onStartCommand: onStartCommand executed");
         return super.onStartCommand(intent, flags, startId);
-    }
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     @Override
@@ -31,4 +33,22 @@ public class MyService extends Service {
         super.onDestroy();
     }
 
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    class DownloadBinder extends Binder {
+        private static final String TAG = "DownloadBinder";
+
+        public void startDownload() {
+            Log.e(TAG, "startDownload: 开启下载");
+        }
+
+        public int getProgress() {
+            Log.e(TAG, "getProgress: 获取进度");
+            return 0;
+        }
+    }
 }
